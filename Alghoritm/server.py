58b662666a2d075f00e45a2023b64218c2e572df
daler0022 <daler_olimov_0055@mail.ru>
2024-05-10 11:55:51 +0500

    abdu

--- Alghoritm/server.py
+++ Alghoritm/server.py
@@ -14,11 +14,18 @@ class FaceServiceServicer(pb2_grpc.FaceServiceServicer):
         db_path = request.db_path
 
         try:
-            print(image_path, db_path)
             current_dir = os.getcwd()
-            img1_path = save_base64_as_file(image_path, os.path.join(current_dir, "image.jpg"))
-            img2_path = save_base64_as_file(db_path, os.path.join(current_dir, "db.jpg"))
-            result = DeepFace.verify(img1_path=img1_path, img2_path=img2_path, model_name="GhostFaceNet")
+            # img1_path = save_base64_as_file(image_path, os.path.join(current_dir, "image.jpg"))
+            # img2_path = save_base64_as_file(db_path, os.path.join(current_dir, "db.jpg"))
+            img1_data = base64.b64decode(image_path)
+            img2_data = base64.b64decode(db_path)
+
+            with open("img1.jpg", "wb") as f:
+                f.write(img1_data)
+
+            with open("img2.jpg", "wb") as f:
+                f.write(img2_data)
+            result = DeepFace.verify("img1.jpg", "img2.jpg", "GhostFaceNet")
             print(result["verified"])
             if(result['verified']):
                 print(result)
